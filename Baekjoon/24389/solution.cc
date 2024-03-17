06518d268d1a94394b92007481c51069457fe3a4
Ju Chanhyoung <ju031230321@gmail.com>
2024-03-17 09:43:32 +0900

    2024/03/17 09:43

---
+++ Baekjoon/24389/solution.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+#define FAST_IO()                        \
+  std::ios_base::sync_with_stdio(false); \
+  std::cin.tie(NULL);                    \
+  std::cout.tie(NULL)
+#define int8 char
+#define int16 short
+#define int32 int
+#define int64 long long
+#define uint8 unsigned int8
+#define uint16 unsigned int16
+#define uint32 unsigned int32
+#define uint64 unsigned int64
+using namespace std;
+
+int main() {
+  FAST_IO();
+
+  int N;
+  cin >> N;
+
+  cout << __builtin_popcount(N ^ (-N));
+
+  return 0;
+}
+
+#undef FAST_IO
